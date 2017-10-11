49d00b04dd80960d4a7b13ac1a8c0fbdad0dc05f
XiaTao <32433843+TaoCurry@users.noreply.github.com>
2017-10-11 19:42:01 +0800

    Update 全局变量和局部变量.py

--- 高阶函数/全局变量和局部变量.py
+++ 高阶函数/全局变量和局部变量.py
@@ -8,4 +8,28 @@ print(num)      #NameError: name 'num' is not defined
 num = 10        #定义在模块最外层的变量是全局变量，它是全局范围内可见的。
 def foo():
     print(num)
+    
+# update
+x = 50
+def func(x):
+    print('x is ', x) 
+    x = 2
+    print('x is ', x)
+func()
+# x is 50
+#x is 2
+print('x is', x)
+# x is 50
 
+#update 
+x =50
+def func(x):
+    global x
+    print('x is', x)
+    x = 2
+    print('x is', x)
+func()
+# x is 50
+# x is 2
+print('x is', x)
+# x is 2
