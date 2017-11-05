7bfda10b3f077d682c786ece571ec87266e60868
XiaTao <32433843+TaoCurry@users.noreply.github.com>
2017-11-05 21:17:04 +0800

    Update 迭代求Fibo.py

--- 递归例子/迭代求Fibo.py
+++ 递归例子/迭代求Fibo.py
@@ -9,3 +9,22 @@ def fibo(max):
 	return 'done'
 for n in fibo(10):
 	print(n)
+
+# Update 更简单的方法计算斐波拉契数列：
+>>> fibs = [0, 1]
+>>> for i in range(8):	 # 
+	fibs.append(fibs[-2] + fibs[-1])
+
+	
+>>> fibs
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+# Update
+>>> num = int(input('Please input Fibo\'s you want: '))
+Please input Fibo's you want: 10
+>>> for i in range(num - 2):
+	fibs.append(fibs[-2] + fibs[-1])
+
+	
+>>> fibs
+[0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
