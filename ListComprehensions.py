c319394c3fcc8e1abb0df222fe12c2fc2ab3f126
XiaTao <32433843+TaoCurry@users.noreply.github.com>
2017-11-05 12:49:24 +0800

    Update ListComprehensions.py

--- ListComprehensions.py
+++ ListComprehensions.py
@@ -1,28 +1,34 @@
 #!/usr/bin/env python 3
 # -*- coding: utf-8 -*-
 #List Comprehensions列表生成式
-list range(1,11)#打印1-10的list
-[x for x in range(1,11)]#把要生成的元素放前面
+
+list range(1, 11)	#打印1-10的list
+[x for x in range(1, 11)]	#把要生成的元素放前面
 #打印1*1，2*2，3*3,...,10*10
 L = []
-for x in range(1,11)：
+for x in range(1, 11)：
 	L.append(x * x)
 print(L)
 
-[x * x for x in range(1,11)]#使用列表生成式
-[x * x for x in range(1,101) if x % 2 == 0]#所有偶数的乘积
-[m + n for m in 'ABC' for n in 'XYZ']#使用两层循环，可以生成全排列：
+[x * x for x in range(1, 11)]	#使用列表生成式
+[x * x for x in range(1, 101) if x % 2 == 0]	#所有偶数的乘积
+[m + n for m in 'ABC' for n in 'XYZ']	#使用两层循环，可以生成全排列：
 #['AX', 'AY', 'AZ', 'BX', 'BY', 'BZ', 'CX', 'CY', 'CZ']
 
-d = {'X':'A','Y':'B','Z':'C'}
-[key + '=' + value for key,value in d.items()]
-#['X=A','Y'='B','Z'='C']
+result = []
+for x in range(3):
+	for y in range(3):
+		return result.append((x, y))
 
-L = ['HELLO','WORLD','APPLE','BMW']
+d = {'X': 'A', 'Y': 'B', 'Z': 'C'}
+[key + '=' + value for key,value in d.items()]
+#['X'='A', 'Y'='B', 'Z'='C']
+
+L = ['HELLO', 'WORLD', 'APPLE', 'BMW']
 [s.lower() for s in L]
 #['hello', 'world', 'ibm', 'apple']
 
-L = ['Hello', 'World', 18, 'Apple', None]#由于非字符串类型没有lower()方法
+L = ['Hello', 'World', 18, 'Apple', None]	#由于非字符串类型没有lower()方法
 [s.lower() for s in L if isinstance(s,str) == True]
 #['hello', 'world', 'apple']
 
