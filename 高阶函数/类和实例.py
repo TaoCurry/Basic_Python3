ba3c249605fc2115a44ec25b00ca0b758dca412f
XiaTao <32433843+TaoCurry@users.noreply.github.com>
2017-11-12 15:21:32 +0800

    Update 类和实例.py

--- 高阶函数/类和实例.py
+++ 高阶函数/类和实例.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 #-*- coding:utf-8 -*-
+# self参数事实上正是方法和函数的区别。方法（绑定方法）将他们的第一个参数绑定到所属的实例上，不需要再次显式提供改参数。
+
 class Student(object):      #Student类, object 继承类
-    def __init__(self, name, score):  #对Student类绑定属性->__init__方法,第一个参数永远为创建实例本身-self.
+    def __init__(self, name, score):  #对Student类绑定属性 __init__ 方法,第一个参数永远为创建实例本身 self.
         self.name = name        #Student类有name和score属性
         self.score = score
     def print_score(self):      #定义一个方法，第一个参数必须是self
@@ -13,9 +15,9 @@ class Student(object):      #Student类, object 继承类
             return 'B'
         else:
             return 'C'
-bart = Student('Bart Simpson', 59)      #创建一个实例，类名+()
+bart = Student('Bart Simpson', 59)      # 创建一个实例，类名+()
 lisa = Student('Lisa Simpson', 91)
-bart.print_score()
+bart.print_score()	 # 实例.方法
 bart.get_grade()
 lisa.print_score()
 lisa.get_grade()
